a48a23559d31286091c4d293fd5b83b842f669eb
jalol <jalolkarshiboyev@gmail.com>
2022-01-24 11:54:27 +0500

    first commit

---
+++ src/main/java/uz/pdp/app5task/App5taskApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.app5task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App5taskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App5taskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/app5task/App5taskApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.app5task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App5taskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
